0a5642c6d1efcf1e9dfe8c577602a52add927687
HOME <daiabxc@gmail.com>
2021-11-22 19:48:13 +0700

    google

--- app/src/main/java/com/example/test_du_an_mau/DangNhapActivity.java
+++ app/src/main/java/com/example/test_du_an_mau/DangNhapActivity.java
@@ -1,29 +1,59 @@
 package com.example.test_du_an_mau;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class DangNhapActivity extends AppCompatActivity {
     private EditText edt_emai, edt_matkhau;
     private Button btn_dangnhap;
-
-
     private FirebaseAuth mAuth;
-
+    private ImageView btn_loginGG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +62,16 @@ public class DangNhapActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-
         TextView tv_dangkyapp = findViewById(R.id.tv_dangkyapp);
         TextView tv_dangky = findViewById(R.id.tv_dangky);
         edt_emai = findViewById(R.id.edt_emai);
         edt_matkhau = findViewById(R.id.edt_matkhau);
         btn_dangnhap = findViewById(R.id.btn_dangnhap);
-
+        btn_loginGG = findViewById(R.id.btn_google);
 
         tv_dangkyapp.setOnClickListener(dangky());
         tv_dangky.setOnClickListener(dangky());
+
 
         btn_dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,8 +80,16 @@ public class DangNhapActivity extends AppCompatActivity {
             }
         });
 
-    }
+        btn_loginGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangNhapActivity.this, loginggActivity.class);
+                startActivity(intent);
+            }
+        });
 
+
+    }
 
     private View.OnClickListener dangky() {
         return new View.OnClickListener() {
@@ -89,4 +127,5 @@ public class DangNhapActivity extends AppCompatActivity {
         });
 
     }
+
 }
