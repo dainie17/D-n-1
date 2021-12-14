2d878f44bbc0db51197fb5da4096a6962cc756ad
HOME <daiabxc@gmail.com>
2021-12-14 14:16:50 +0700

    phản hồi

---
+++ app/src/main/java/com/example/test_du_an_mau/Activity/PhanHoiActivity.java
@@ -0,0 +1,16 @@
+package com.example.test_du_an_mau.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test_du_an_mau.R;
+
+public class PhanHoiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phan_hoi);
+    }
+}
